d46beb029db60807865602dc1056fbbe30d1ddfe
Rijiel <rijielremulla@gmail.com>
2025-01-09 22:00:11 +0800

    Create repository services.

---
+++ src/SimpleCRUD.Core/Domain/RepositoryContracts/IPersonRepository.cs
@@ -0,0 +1,25 @@
+﻿using SimpleCrud.Core.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCRUD.Core.Domain.RepositoryContracts;
+public interface IPersonRepository
+{
+	Task<IEnumerable<Person>> GetAllAsync(Expression<Func<Person, bool>>? expression = null);
+
+	Task<Person?> GetAsync(Expression<Func<Person, bool>> expression, bool tracked = false);
+
+	Task AddAsync(Person person);
+
+	Task AddRangeAsync(IEnumerable<Person> persons);
+
+	Task UpdateAsync(Person person);
+
+	Task DeleteAsync(Person person);
+
+	Task RemoveRangeAsync(IEnumerable<Person> persons);
+}
---
+++ src/SimpleCRUD.Infrastructure/Repositories/PersonRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCrud.Core.Domain.Models;
+using SimpleCRUD.Core.Domain.RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCRUD.Infrastructure.Repositories;
+public class PersonRepository : IPersonRepository
+{
+	private readonly ApplicationDbContext _context;
+
+	public PersonRepository(ApplicationDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<IEnumerable<Person>> GetAllAsync(Expression<Func<Person, bool>>? expression = null)
+	{
+		return await _context.Persons.Where(expression?? (x => true)).ToListAsync();
+	}
+
+	public async Task<Person?> GetAsync(Expression<Func<Person, bool>> expression, bool tracked = false)
+	{
+		return await (
+			tracked ? _context.Persons 
+			: _context.Persons.AsNoTracking())
+			.FirstOrDefaultAsync(expression);
+	}
+
+	public async Task AddAsync(Person person)
+	{
+		_context.Persons.Add(person);
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task AddRangeAsync(IEnumerable<Person> persons)
+	{
+		_context.Persons.AddRange(persons);
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task DeleteAsync(Person person)
+	{
+		_context.Persons.Remove(person);
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task RemoveRangeAsync(IEnumerable<Person> persons)
+	{
+		_context.Persons.RemoveRange(persons);
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task UpdateAsync(Person person)
+	{
+		_context.Persons.Update(person);
+		await _context.SaveChangesAsync();
+	}
+}
