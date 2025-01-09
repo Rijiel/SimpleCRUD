8e573cedebd687f995ec5586bfb0b66fe2d56f05
Rijiel <rijielremulla@gmail.com>
2025-01-09 21:06:13 +0800

    Restructure class libraries.

--- src/SimpleCrud.Core/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SimpleCrud.Core;
-
-public class Class1
-{
-
-}
---
+++ src/SimpleCrud.Core/Domain/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCrud.Core.Domain.Models;
+
+public class Person
+{
+	public int Id { get; set; }
+
+	[Required]
+	[StringLength(50)]
+	public string FirstName { get; set; } = null!;
+
+	[Required]
+	[StringLength(50)]
+	public string LastName { get; set; } = null!;
+
+	[EmailAddress]
+	[StringLength(50)]
+	public string? Email { get; set; } = null!;
+
+	[Required]
+	[Range(1, 120)]
+	public int Age { get; set; }
+}
